2bb0a54c0414ea82ee8b426362ca3dd2d2abc23b
ronimizy <ronimizy@outlook.com>
2024-10-06 22:33:09 +0300

    fix: registered repository

--- src/Lab2.Tools.Persistence/Extensions/ServiceCollectionExtensions.cs
+++ src/Lab2.Tools.Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
 using Itmo.Dev.Platform.Persistence.Abstractions.Extensions;
 using Itmo.Dev.Platform.Persistence.Postgres.Extensions;
+using Lab2.Tools.Abstractions.Persistence;
+using Lab2.Tools.Persistence.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Lab2.Tools.Persistence.Extensions;
@@ -12,6 +14,8 @@ public static class ServiceCollectionExtensions
             .UsePostgres(postgres => postgres
                 .WithConnectionOptions(builder => builder.BindConfiguration("Persistence:Postgres"))
                 .WithMigrationsFrom(typeof(IAssemblyMarker).Assembly)));
+
+        collection.AddScoped<IConfigurationRepository, ConfigurationRepository>();
 
         return collection;
     }
